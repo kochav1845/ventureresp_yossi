babb45f08e2214125ce4aba28fbdf3375bd8d119
kochav1845 <a88933513@gmail.com>
2026-03-05 18:51:03 +0000

    Updated index.ts

--- supabase/functions/send-customer-invoice-email/index.ts
+++ supabase/functions/send-customer-invoice-email/index.ts
@@ -198,10 +198,6 @@ Deno.serve(async (req: Request) => {
         email: 'invoices@starwork.dev',
         name: 'Venture Respiratory - Accounts Receivable',
       },
-      reply_to: {
-        email: 'invoices@inbound.starwork.dev',
-        name: 'Venture Respiratory - Accounts Receivable',
-      },
       content: [
         {
           type: 'text/html',
